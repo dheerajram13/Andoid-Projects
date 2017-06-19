a463172864f4e5d957f7482f549f139ddeaae560
dheerud13 <rcharan10@gmail.com>
2017-06-19 19:19:28 +0530

    Stopwatch

---
+++ Stopwatch/app/src/main/java/com/example/dheeraj/stopwatch/stopwatch.java
@@ -0,0 +1,97 @@
+package com.example.dheeraj.stopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.os.Handler;
+public class stopwatch extends AppCompatActivity {
+
+    private int seconds=0;
+    private boolean running;
+    private boolean wasRunning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        if(savedInstanceState!=null){
+            seconds=savedInstanceState.getInt("seconds");
+            running= savedInstanceState.getBoolean("running");
+            wasRunning=savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
+    }
+     @Override
+     public void onSaveInstanceState(Bundle savedInstanceState){
+         savedInstanceState.putInt("seconds", seconds);
+         savedInstanceState.putBoolean("running", running);
+         savedInstanceState.putBoolean("wasRunning", wasRunning);
+     }
+    @Override
+    public void onStop(){
+        super.onStop();
+        wasRunning = running;
+        running =false;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+       if( wasRunning){
+           running=true;
+       }
+    }
+    public  void onClickStart(View view){
+        running=true;
+    }
+    public void onClickStop(View view){
+        running=false;
+    }
+    public void onClickReset(View view ){
+        running =false;
+        seconds=0;
+    }
+
+    public void runTimer(){
+        final TextView timeView =(TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run () {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_stopwatch, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
